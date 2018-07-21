729b4a86732cb4c69a897c3ce64844ceecc6e555
Belal Khan <probelalkhan@gmail.com>
2018-07-21 22:05:29 +0530

    Basic Authentication

--- app/src/main/java/net/simplifiedcoding/retrofitandroidtutorial/api/RetrofitClient.java
+++ app/src/main/java/net/simplifiedcoding/retrofitandroidtutorial/api/RetrofitClient.java
@@ -1,19 +1,47 @@
 package net.simplifiedcoding.retrofitandroidtutorial.api;
 
+import android.util.Base64;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
+    private static final String AUTH = "Basic " + Base64.encodeToString(("belalkhan:123456").getBytes(), Base64.NO_WRAP);
+
     private static final String BASE_URL = "http://192.168.43.144/MyApi/public/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
+
     private RetrofitClient() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(
+                        new Interceptor() {
+                            @Override
+                            public Response intercept(Chain chain) throws IOException {
+                                Request original = chain.request();
+
+                                Request.Builder requestBuilder = original.newBuilder()
+                                        .addHeader("Authorization", AUTH)
+                                        .method(original.method(), original.body());
+
+                                Request request = requestBuilder.build();
+                                return chain.proceed(request);
+                            }
+                        }
+                ).build();
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
     }
 
@@ -24,7 +52,7 @@ public class RetrofitClient {
         return mInstance;
     }
 
-    public Api getApi(){
+    public Api getApi() {
         return retrofit.create(Api.class);
     }
 }
